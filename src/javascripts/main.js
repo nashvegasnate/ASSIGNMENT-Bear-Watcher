797bd0215e5fe75eb1274530d2073d61725f56a7
Nathan Keith <nashvegasnate@gmail.com>
2021-02-22 15:16:02 -0600

    converted HTML files to JS functions and emptied index.HTML, renamed functions: bearBuilder=river, form=bearForm

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import '../styles/main.scss';
 
 const bears = [
   {
-    name: 'Yogi',
+    name: 'Flash',
     imageUrl: 'https://images.pexels.com/photos/35435/pexels-photo.jpg?cs=srgb&dl=pexels-rasmus-svinding-35435.jpg&fm=jpg',
   },
   {
@@ -17,6 +17,7 @@ const bears = [
     name: 'Louise',
     imageUrl: 'https://www.publicdomainpictures.net/pictures/230000/velka/brown-bear-1504708649cqE.jpg',
   },
+
 ];
 
 const printToDom = (divId, textToPrint) => {
@@ -24,17 +25,39 @@ const printToDom = (divId, textToPrint) => {
   selectedDiv.innerHTML = textToPrint;
 };
 
-const bearBuilder = () => {
+const bearForm = () => {
+  const domString = `<div class="jumbotron jumbotron-fluid">
+    <div class="container" div id="jumboTron">
+    <h1 class="display-4">Bear Watcher</h1>
+    <p class="lead">Keep track of your favorite bears in Katmai National Park!</p>
+    <form>
+      <h3>ENTER NEW BEAR INFORMATION BELOW</h3>
+      <div class="form-label" div id="bearForm">
+        <label for="name">Name of New Bear</label>
+        <input type="text" class="form-control form-control-lg" placeholder="enter name of new bear" id="name" required>
+        <input type="url" class="form-control form-control" id="imageUrl" placeholder="enter image URL" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  <div class="container mt-3 text-center">
+    <div id="bearList"> 
+  </div>
+</div>`;
+  printToDom('body', domString);
+};
+
+const river = () => {
   let domString = '';
   bears.forEach((item, i) => {
-    domString += `<div class="card" style="width: 18rem;" id=${i}>
-                    <img class="card-img-top" img src="url('${item.imageUrl}');"></img>
+    domString += `<div class="card" style="width: 18rem;" id="${i}">
+                    <img class="card-img-top" img src="${item.imageUrl}"></img>
                     <div class="card-body">
                       <p class="card-text">${item.name}</p>
                     </div>
                   </div>`;
   });
-  printToDom('#bears', domString);
+  printToDom('#bearList', domString);
 };
 
 const getFormInfo = (e) => {
@@ -49,18 +72,19 @@ const getFormInfo = (e) => {
 
   bears.push(obj);
 
-  bearBuilder(bears);
+  river(bears);
 
   document.querySelector('form').reset();
 };
 
-const buttonEvents = () => {
+const domEvents = () => {
   document.querySelector('form').addEventListener('submit', getFormInfo);
 };
 
 const init = () => {
-  buttonEvents();
-  bearBuilder(bears);
+  bearForm();
+  river(bears);
+  domEvents();
 };
 
 init();
